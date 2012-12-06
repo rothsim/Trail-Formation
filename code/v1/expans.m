8c642b631ca63e31a42d58300ed66b619f45709d
Simon Roth <git@simonroth.ch>
2012-12-06 20:26:51 +0100

    clean up

--- code/v1/expans.m
+++
@@ -1,7 +0,0 @@
-function [ fQ ] = expans( Q, g )
-%EXPANS computes the expansion of tubes.
-%   This function describes the expansion of tubes in response to the flux.
-
-    fQ = abs( Q )^g/( 1+abs( Q )^g );
-
-end
--- code/v1/varnum.m
+++
@@ -1,9 +0,0 @@
-function [ num ] = varnum( y, x, X )
-%VARNUM assigns a unique number to every node or junctionnode.
-%   A unique number for every node or junctionnode is nessecary to generate
-%   and solve the equation system. The input variables are the coordinates
-%   x y and the X dimension of the simulation surface.
-
-    num = ( y-1 )*X+x;
-
-end
